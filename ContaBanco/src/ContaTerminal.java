db09e7d7caf056855886c58675ae00c503454246
HelintonAlves <helinton-alves@hotmail.com>
2024-04-26 16:48:13 -0300

    Melhorando a visualização do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -12,9 +12,6 @@ import java.util.Scanner;
  * 
  */
 
-
-
-
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TODO: Conhecer e importar a classe Scanner
