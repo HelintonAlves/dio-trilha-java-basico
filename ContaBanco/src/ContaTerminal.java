0651f28e3190ac7f45a22647565460ad3d0c7947
HelintonAlves <helinton-alves@hotmail.com>
2024-04-26 16:21:55 -0300

    commit com a implementação e funcionamento do código

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,45 @@
+
+import java.util.Scanner;
+
+/**
+ * Desafio de código do Boot Camp da DIO - Criação de um terminal de conta bancária
+ * @author Helinton Alves Coelho
+ * @version 1.0.0
+ * @since 26/04/2024
+ * 
+ * Na implementação observei que o campo saldo utiliza o padrão brasileiro,
+ * ou seja, utilização de vírgula nas casas décimais e não ponto. 
+ * 
+ */
+
+
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        // Exibir as mensagens para o nosso usuário
+        // Obter pela classe Scanner o valores digitados no terminal
+        //Exibir a mensagem conta criada
+
+        int numeroConta;
+        String agencia;
+        String cliente;
+        double saldo;
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Por favor, digite o número de sua conta: ");
+        numeroConta = scanner.nextInt();
+
+        System.out.println("Agora digite a Agência: ");
+        agencia = scanner.next();
+
+        cliente = scanner.next();
+
+        System.out.println("Digite o valor de seu depósito: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
